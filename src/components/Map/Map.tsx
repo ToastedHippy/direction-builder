6bb7c676532fb49c2309270abd65c0102a9590a0
ToastedHippy <komyagin.alexey@gmail.com>
2018-02-26 23:07:07 +0500

    got google maps

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { } from "@types/googlemaps";
 import { IPoint } from "../../models/Point";
 import "./Map.scss";
 
@@ -6,16 +7,36 @@ export interface IMapProps {
   points: IPoint[];
 }
 
+const API_KEY = "AIzaSyA2E6gObCM5rS5SB1apN5C2szQHjlHg084";
+const GOOGLE_MAP_API_URL = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}`;
+
 export default class Map extends React.Component<IMapProps, any> {
 
   constructor(props: IMapProps) {
     super(props);
   }
 
+  initGoogleMap = () => {
+    console.log(google.maps.Map);
+  }
+
+  createGMScript() {
+    const script = document.createElement("script");
+    script.src = GOOGLE_MAP_API_URL;
+    script.async = true;
+
+    script.addEventListener("load", this.initGoogleMap);
+
+    return script;
+  }
+
+  componentDidMount() {
+    document.body.appendChild(this.createGMScript());
+  }
+
   render() {
     return (
       <div className="map">
-
       </div>
     );
   }
